1ce1172809bbf541a18664cdea174c3a5471d090
aymen <aymen.sellaouti@gmail.com>
2020-03-19 15:33:12 +0100

    Updates

---
+++ precache-manifest.b10278f81c5e82e6ca0249ff4854ea81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4b0c98d25e484803fed30f9a07d6407",
+    "url": "/firstReact/index.html"
+  },
+  {
+    "revision": "edf05a42a81041b91691",
+    "url": "/firstReact/static/css/main.4a8bf6e9.chunk.css"
+  },
+  {
+    "revision": "2b7d9edd948003b96165",
+    "url": "/firstReact/static/js/2.b6796cae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/firstReact/static/js/2.b6796cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf05a42a81041b91691",
+    "url": "/firstReact/static/js/main.55988bf5.chunk.js"
+  },
+  {
+    "revision": "d946a30d210f983767a0",
+    "url": "/firstReact/static/js/runtime-main.9bb9eee3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
